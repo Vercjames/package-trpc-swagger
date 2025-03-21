684c9d76bf886a4482ece046949b0f4a14be5f98
James R. Maxfield <Vercjames@gmail.com>
2025-03-21 14:05:50 -0500

    setup

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,9 @@
-import { prisma } from "@repo/database";
-
 export default async function IndexPage() {
-  const users = await prisma.user.findMany();
 
   return (
     <div>
       <h1>Hello World</h1>
-      <pre>{JSON.stringify(users, null, 2)}</pre>
+      <pre></pre>
     </div>
   );
 }
