0d23ee133a86754213153d90e8ff06d953b029bc
James R. Maxfield <Vercjames@gmail.com>
2025-03-30 18:30:40 -0500

    rigging

---
+++ examples/with-nextjs-app/server/openapi.ts
@@ -0,0 +1,13 @@
+import { generateOpenApiDocument } from "@repo/trpc-swagger"
+
+// import { appRouter } from "./router"
+
+// Generate OpenAPI schema document
+export const openApiDocument = generateOpenApiDocument(_, {
+  title: "Example CRUD API",
+  description: "OpenAPI compliant REST API built using tRPC with Next.js",
+  version: "1.0.0",
+  baseUrl: "http://localhost:3000/api",
+  docsUrl: "https://github.com/vercjames/package-trpc-swagger",
+  tags: ["auth", "users", "posts"],
+})
