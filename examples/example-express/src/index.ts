173252dca6ace62a48dcc62b423903ce88604207
James R. Maxfield <Vercjames@gmail.com>
2026-06-10 09:46:32 -0500

    stashing

--- examples/example-express/src/index.ts
+++ examples/example-express/src/index.ts
@@ -1,12 +1,14 @@
+import { VERSION } from "trpc-swagger"
 import express from "express"
 
 const app = express()
-const port = 3001
+const port = 3002
 
 app.get("/", (_req, res) => {
-  res.json({ message: "example-express is running" })
+  res.json({ message: `rawr example-express is running ${VERSION}` })
 })
 
 app.listen(port, () => {
+  console.log(`trpc-swagger v${VERSION}`)
   console.log(`Express server listening on http://localhost:${port}`)
 })
